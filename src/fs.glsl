dce353c7e2ca779081a12f96b661ad60546dd041
Antoine <antoine.dupuis@etudiant.univ-rennes1.fr>
2022-09-05 00:32:40 +0200

    mipmap alpha bleeding band aid

--- src/fs.glsl
+++ src/fs.glsl
@@ -4,11 +4,10 @@ varying highp vec3 vLighting;
 uniform sampler2D uSampler;
 
 void main(void) {
-    highp vec4 texelColor = texture2D(uSampler, vTextureCoord);
     // Get some light on black texel
-    texelColor = texelColor + vec4(0.003, 0.003, 0.003, 0);
-    if(texelColor.a == 0.0)
-        texelColor = vec4(1.0, 1.0, 1.0, 1.0);
+    highp vec4 texelColor = texture2D(uSampler, vTextureCoord) + vec4(0.01, 0.01, 0.01, 0);
+    highp float coeff = 1.0 - texelColor.a;
+    texelColor = texelColor + vec4(1.0, 1.0, 1.0, 1.0) * coeff;
 
     gl_FragColor = vec4(texelColor.rgb * vLighting, texelColor.a);
 }
